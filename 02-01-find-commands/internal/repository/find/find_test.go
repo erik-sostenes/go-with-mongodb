769fdd4d24d4fdafd42002a507f3df155fed232f
erik-sostenes-simon <eriksostenessimon@gmail.com>
2022-08-10 13:02:19 -0500

    Using the *mongo.Cursor.All function.
    issue #02

--- 02-01-find-commands/internal/repository/find/find_test.go
+++ 02-01-find-commands/internal/repository/find/find_test.go
@@ -3,40 +3,64 @@ package find
 import (
 	"context"
 	"errors"
-	"os"
 	"reflect"
-	"strconv"
 	"testing"
-	"time"
 
 	"github.com/erik-sostenes/curso-mongo/internal/model"
 	"github.com/erik-sostenes/curso-mongo/internal/repository"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func TestFindAllAccounts(t *testing.T) {
-	timeout, _ := strconv.Atoi(os.Getenv("NoSQL_TIMEOUT"))
-
-	config := repository.MongoDB{
-		Uri:            "mongodb+srv://%s:%s@cluster0.o3tc8ee.mongodb.net/?retryWrites=true&w=majority",
-		User:           os.Getenv("NoSQL_USER"),
-		Password:       os.Getenv("NoSQL_PASSWORD"),
-		DatabaseName:   os.Getenv("NoSQL_DATABASE"),
-		ConnectTimeout: time.Duration(timeout),
-	}
+func TestFindNextAccount(t *testing.T) {
 	tsc := map[string]struct {
-		accounts         Account
+		accounts         account
 		expectedAccounts model.Accounts
 		expectedError    error
 	}{
 		"Given a collection that exists, return a list of accounts": {
 			accounts: NewAccount(
-				repository.NewMDB(config).Collection("accounts"),
+				repository.NewMDB(repository.Config).Collection("accounts"),
 			),
 		},
 		"Given a collection that does not exist, return no account": {
 			accounts: NewAccount(
-				repository.NewMDB(config).Collection("some_collection"),
+				repository.NewMDB(repository.Config).Collection("some_collection"),
+			),
+			expectedError: mongo.ErrNoDocuments,
+		},
+	}
+
+	for name, ts := range tsc {
+		t.Run(name, func(t *testing.T) {
+			accounts, err := ts.accounts.FindNextAccount(context.TODO())
+			if err != nil {
+				if !errors.Is(err, ts.expectedError) {
+					t.Fatalf("expected error %v, got %v error", ts.expectedError, err)
+				}
+				t.SkipNow()
+			}
+
+			if reflect.TypeOf(accounts) != reflect.TypeOf(ts.expectedAccounts) {
+				t.Fatalf("expected %T, got %T", ts.expectedAccounts, ts.accounts)
+			}
+		})
+	}
+}
+
+func TestFindAllAccounts(t *testing.T) {
+	tsc := map[string]struct {
+		accounts         account
+		expectedAccounts model.Accounts
+		expectedError    error
+	}{
+		"Given a collection that exists, return a list of accounts": {
+			accounts: NewAccount(
+				repository.NewMDB(repository.Config).Collection("accounts"),
+			),
+		},
+		"Given a collection that does not exist, return no account": {
+			accounts: NewAccount(
+				repository.NewMDB(repository.Config).Collection("some_collection"),
 			),
 			expectedError: mongo.ErrNoDocuments,
 		},
@@ -56,5 +80,21 @@ func TestFindAllAccounts(t *testing.T) {
 				t.Fatalf("expected %T, got %T", ts.expectedAccounts, ts.accounts)
 			}
 		})
+	}
+}
+
+func BenchmarkFindNextAccount(b *testing.B) {
+	account := NewAccount(repository.NewMDB(repository.Config).Collection("accounts"))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		account.FindNextAccount(context.TODO())
+	}
+}
+
+func BenchmarkFindAllAccounts(b *testing.B) {
+	account := NewAccount(repository.NewMDB(repository.Config).Collection("accounts"))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		account.FindNextAccount(context.TODO())
 	}
 }
--- 02-01-find-commands/internal/repository/repository.go
+++ 02-01-find-commands/internal/repository/repository.go
@@ -1,6 +1,10 @@
 package repository
 
-import "time"
+import (
+	"os"
+	"strconv"
+	"time"
+)
 
 // MongoDB contains the environment variables to configure the mongodb connection
 type MongoDB struct {
@@ -10,3 +14,15 @@ type MongoDB struct {
 	DatabaseName string
 	ConnectTimeout time.Duration
 }
+
+var (
+	timeout, _ = strconv.Atoi(os.Getenv("NoSQL_TIMEOUT"))
+	// Config settings to a mongodb connection
+	Config = MongoDB{
+		Uri:            "mongodb+srv://%s:%s@cluster0.o3tc8ee.mongodb.net/?retryWrites=true&w=majority",
+		User:           os.Getenv("NoSQL_USER"),
+		Password:       os.Getenv("NoSQL_PASSWORD"),
+		DatabaseName:   os.Getenv("NoSQL_DATABASE"),
+		ConnectTimeout: time.Duration(timeout),
+	}
+)
